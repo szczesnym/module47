4a6a679f628fae63966fb18966c8bf08659c9851
szczesnym <marcins@oerlemans-foods.nl>
2018-04-27 10:35:47 +0200

    Ex. 7.5 IntStream

--- src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -8,7 +8,6 @@ public class ArrayOperationsTestSuite {
     public void testArrayAvg() {
         //Given
         int[] sutTabOfInt = {1, 2, 3, 4, 5, 6, 7, 8, 9, 11};
-
         //When
         double testResult = ArrayOperations.getAverage(sutTabOfInt);
         //Then
